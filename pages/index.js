f3135f1dcc0f74bdf984e8ea7dcb1c99873d534c
Shova Sarker <shova.sarker@vivacomsolutions.com>
2023-05-31 14:53:06 +0600

    Added page title

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Banner from '../components/Banner';
 import Header from '../components/Header';
 import Table from '../components/Table';
@@ -5,6 +6,9 @@ import Table from '../components/Table';
 export default function Home() {
   return (
     <div className='container mx-auto'>
+      <Head>
+        <title>Practicing next js</title>
+      </Head>
       <Header />
       <main className='p-5'>
         <Banner />
